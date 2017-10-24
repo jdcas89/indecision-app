5dadcec5562d48a4adcd6b6eaffec5549af961c4
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-24 17:47:59 +0200

    Arrow functions part 1

--- scripts/app.js
+++ scripts/app.js
@@ -1,80 +1,24 @@
 'use strict';
 
-console.log('App.js is running');
-
-var app = {
-  title: 'Indecision app',
-  subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+var squareFunction = function squareFunction(x) {
+  console.log(this);
+  return x * x;
+};
+var square = function square(x) {
+  console.log(undefined);
+  return x * x;
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    ' ',
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    ' ',
-    app.subtitle,
-    ' '
-  ),
-  app.options && app.options.length > 0 ? React.createElement(
-    'p',
-    null,
-    'Here are your options:'
-  ) : React.createElement(
-    'p',
-    null,
-    ' No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement('li', null)
-  )
-);
+var shortArrowSquare = function shortArrowSquare(x) {
+  return x * x;
+};
+console.log(shortArrowSquare(8));
 
-var user = {
-  name: 'JD',
-  age: 25,
-  location: 'The world'
+var getFirstName = function getFirstName(fullName) {
+  return fullName.split(' ')[0];
+};
+var shortGetFirstName = function shortGetFirstName(fullName) {
+  return fullName.split(' ')[0];
 };
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      ' Location: ',
-      location
-    );
-  }
-}
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    ' ',
-    user.name ? user.name.toUpperCase() : 'Anonymous'
-  ),
-  user.age && user.age > 18 && React.createElement(
-    'p',
-    null,
-    ' Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(getFirstName('John Doe'));
