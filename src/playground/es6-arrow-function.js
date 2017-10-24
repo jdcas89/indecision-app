875abd219f7dc2a0f2f5c9826409a0ebe14925c0
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-24 17:47:59 +0200

    Arrow functions part 1

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,20 @@
+
+
+const squareFunction = function(x) {
+  console.log(this);
+  return x * x;
+}
+const square = (x) => {
+  console.log(this);
+  return x * x;
+}
+
+const shortArrowSquare = (x) =>  x * x
+console.log(shortArrowSquare(8))
+
+const getFirstName =  (fullName) =>{
+  return fullName.split(' ')[0]
+} 
+const shortGetFirstName =  (fullName) => fullName.split(' ')[0]
+
+console.log(getFirstName('John Doe'))
