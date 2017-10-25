1cb44f397d39d6adb3374e4e0475819cc5850dbc
dev <dev@admins-iMac.local>
2017-10-25 20:02:19 +0200

    build it challenge

--- scripts/app.js
+++ scripts/app.js
@@ -1,103 +1,44 @@
-"use strict";
-
-console.log("App.js is running");
+'use strict';
 
 var app = {
-  title: "Indecision app",
-  subtitle: "Put your life in the hands of a computer",
-  options: []
+  title: 'Visibility toggle',
+  showingDetails: false
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
+var buttonText = 'Show details';
 
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = "";
+var onShowDetails = function onShowDetails() {
+  if (app.showingDetails) {
+    app.showingDetails = !app.showingDetails;
+    buttonText = 'Show details';
+  } else {
+    app.showingDetails = !app.showingDetails;
+    buttonText = 'Hide details';
   }
   render();
 };
 
-var onRemoveAll = function onRemoveAll() {
-
-  app.options = [];
-  render();
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-
-  var option = app.options[randomNum];
-  alert(option);
-};
-
-//https://reactjs.org/docs/events.html
-
 var appRoot = document.getElementById("app");
-
-//id gets bound just fine but class = className
-// templateTwo turns into a object. console.log(this) to show dom elements
-// https://reactjs.org/docs/dom-elements.html
-// html attributes that are reserved but supported are camelcased
 
 var render = function render() {
   var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-      "h1",
+      'h1',
       null,
-      " ",
+      ' ',
       app.title
     ),
-    app.subtitle && React.createElement(
-      "p",
-      null,
-      " ",
-      app.subtitle,
-      " "
-    ),
-    app.options && app.options.length > 0 ? React.createElement(
-      "p",
-      null,
-      "Here are your options:"
-    ) : React.createElement(
-      "p",
-      null,
-      " No options"
+    React.createElement(
+      'button',
+      { onClick: onShowDetails },
+      buttonText
     ),
     React.createElement(
-      "ol",
-      null,
-      app.options.map(function (option, index) {
-        return React.createElement(
-          "li",
-          { key: option },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add Option"
-      )
-    ),
-    React.createElement(
-      "button",
-      { onClick: onRemoveAll },
-      "Remove all"
-    ),
-    React.createElement(
-      "button",
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      "What should I do?"
+      'p',
+      { hidden: !app.showingDetails },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lorem et nisl sollicitudin venenatis nec eget elit. Maecenas in eleifend massa, feugiat consectetur justo. Vivamus at hendrerit massa, vitae commodo magna. Nullam luctus metus eu mauris imperdiet, at congue ex posuere.'
     )
   );
   ReactDOM.render(template, appRoot);
