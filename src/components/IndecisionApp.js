50929cf7ed31a6ac0f9f0b3c1b37c5989ebc308b
J.D C <jd.cast@outlook.com>
2017-11-05 21:52:14 +0100

    webpack dev server

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -75,7 +75,7 @@ export default class IndecisionApp extends React.Component {
 
   render() {
     const title = "Indecision";
-    const subtitle = "Put your life in the hands of a computer.";
+    const subtitle = "Put your life in the hands of a computer!";
 
     return (
       <div>
